e4e853a328b7910fa411d18c34461fa4d1fb6e61
Germaine P <glpannell@icloud.com>
2017-04-07 13:30:42 -0400

    updated comments to clearly state what going on in the program

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,12 +1,15 @@
 import media
 import fresh_tomatoes
 
+#the movie instances only require the name an youtube trailer  url
 crash = media.Movie('Crash', 'https://www.youtube.com/watch?v=durNwe9pL0E')
 troy = media.Movie('Troy', 'https://youtu.be/QpikTTSOHXc')
 dark_knight = media.Movie('The Dark Knight', 'https://youtu.be/EXeTwQWrcwY')
 iron_man = media.Movie('Iron Man', 'https://youtu.be/8OCJRbzdzaU')
+
+#setting movies to a list 
 movie_list = [crash, troy, dark_knight, iron_man]
 
 
-
+#sending the list of movie to the frsh tomatos module
 fresh_tomatoes.open_movies_page(movie_list)
--- media.py
+++ media.py
@@ -1,15 +1,17 @@
 import json
 import requests
 
+#looks up the movie from the imdb api
 def movie_lookup(movie_title):
     url = "http://www.omdbapi.com/?t=" + movie_title
     response = requests.get(url)
-    data = json.loads(response.text)
+    data = json.loads(response.text) #parses json data
     return data
 
 class Movie(): 
   def  __init__(self, title, trailer_youtube_url):
     self.title = title
+    #passes the title to movie_look up and returns the Plot and Poster Art 
     self.plot = movie_lookup(title)['Plot']
     self.poster_image_url = movie_lookup(title)['Poster']
     self.trailer_youtube_url = trailer_youtube_url
